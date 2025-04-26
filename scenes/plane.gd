769c13f96def054ac510d163285373a8d4465a00
Alexander Mogilnikov <aleksandr.mogilnikov@udv.group>
2025-04-26 18:59:13 +0500

    water_test

--- scenes/plane.gd
+++ scenes/plane.gd
@@ -9,7 +9,13 @@ extends RigidBody3D
 
 @onready var probes = $Probes.get_children()
 
+@export var speed = 10.;
+@export var turn_speed = 10.;
+@export var turn_speed_down = 10;
+
 var submerged := false
+
+var direction:Vector3;
 
 
 # Called when the node enters the scene tree for the first time.
@@ -19,9 +25,26 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	direction = Vector3()
+	if Input.is_action_pressed("ui_right"):
+		direction.x += 1
+	if Input.is_action_pressed("ui_left"):
+		direction.x -= 1
+	if Input.is_action_just_pressed("ui_down"):
+		print("true")
+		freeze = true
+	if Input.is_action_just_released("ui_down"):
+		print("false")
+		freeze = false
+
 
 func _physics_process(delta):
+	apply_force(Vector3.FORWARD * delta * speed, Vector3.ZERO)
+	if direction.x != 0:
+		print(direction.x)
+		apply_force(Vector3(direction*delta*turn_speed + Vector3.DOWN *delta*turn_speed_down),
+		 direction*2)
+	
 	submerged = false
 	for p in probes:
 		var depth = water.get_height(p.global_position) - p.global_position.y 
@@ -33,3 +56,4 @@ func _integrate_forces(state: PhysicsDirectBodyState3D):
 	if submerged:
 		state.linear_velocity *=  1 - water_drag
 		state.angular_velocity *= 1 - water_angular_drag 
+	
