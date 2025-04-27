aec6cc2ffb199940c3d4e9379696aeadf26167bd
GangBoss <49919388+GangBoss@users.noreply.github.com>
2025-04-27 08:30:34 +0500

    add main menu

--- scenes/canon/enemy_canon.gd
+++ scenes/canon/enemy_canon.gd
@@ -7,20 +7,15 @@ var can_shoot:bool=true
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	print("found target"+str(body.global_position) +str(body.name))
 	current_target=body
 	has_target = true
 
 func _on_area_3d_body_exited(body: Node3D) -> void:
-	print("exited target"+str(body.global_position)+str(body.name))
 	has_target = false
 
 func _process(delta: float):
 	super(delta)
 	if is_shooting == false && has_target==true && can_shoot==true :
-		print("is_shooting" + str(is_shooting))
-		print("has_target" + str(has_target))
-		print("can_shoot" + str(can_shoot))
 		var random= randi()%200 -2
 		random=0
 		shoot(current_target.global_position+ Vector3(0,0,random))
@@ -28,7 +23,6 @@ func _process(delta: float):
 
 func _on_shooted() -> void:
 	if can_shoot ==true :
-		print("signal")
 		can_shoot=false
 		await get_tree().create_timer(delay_before_shoot).timeout
 		can_shoot=true
--- scenes/canon/projectyle.gd
+++ scenes/canon/projectyle.gd
@@ -5,11 +5,9 @@ extends RigidBody3D
 var _timer: float = 0.0
 
 func _ready():
-	print("projectyle position" + str(self.global_position))
 	_timer = lifetime
 
 func _physics_process(delta):
 	_timer -= delta
-	print("projectyle position" + str(self.global_position))
 	if _timer <= 0.0:
 		queue_free() 
